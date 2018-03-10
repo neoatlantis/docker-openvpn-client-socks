9efd49df095f7567af6725b9a12c4d842e9702e6
Chaobai Li <contact@chaobai.li>
2018-03-10 02:09:59 +0100

    more robust docker management: deterministic docker name, kill existing docker before execution, and external connectivity monitoring

--- vpngate.py
+++ vpngate.py
@@ -9,16 +9,54 @@ import base64
 import random
 import tempfile
 import subprocess
+import hashlib
+
+# TODO: detect connectivity in this script instead of docker inside.
 
 from start_single import buildStartCommand
+
+
 
 _BASEPATH = os.path.realpath(os.path.dirname(sys.argv[0]))
 LOCALPATH = lambda *i: os.path.join(_BASEPATH, *i)
 CACHE_TIME = 7200
+TIMEOUT = 30
+INTERVAL = 10
+
 
 if len(sys.argv) < 2:
-    print("Usage: python3 vpngate.py <LOCAL_ADDR>:<LOCAL_PORT>")
+    print("Usage: python3 vpngate.py [<LOCAL_ADDR>:]<LOCAL_PORT>")
     exit(1)
+
+PROXY = sys.argv[1]
+if ":" not in PROXY:
+    PROXY = "127.0.0.1:" + PROXY
+
+##############################################################################
+
+def testConnectivity(proxy):
+    global TIMEOUT
+    try:
+        subprocess.check_output([
+            "curl",
+            "--proxy", "socks5h://%s" % proxy,
+            "--max-time", str(TIMEOUT),
+            "--head",
+            "--silent",
+            "www.google.com"
+        ])
+        return True
+    except FileNotFoundError:
+        print("Install `curl` for testing connectivity.")
+        exit(1)
+    except:
+        return False
+
+def getName(target):
+    return "vpngate-" + \
+        base64.b32encode(\
+            hashlib.sha512(target.encode('utf-8')).digest()[:12]
+        ).decode('ascii').rstrip("=")
 
 def getList():
     global CACHE_TIME
@@ -58,6 +96,21 @@ def read(l):
     except:
         return None
 
+def killDocker(name):
+    print("Try to kill and remove docker container [%s]." % name)
+    try:
+        subprocess.run(["docker", "container", "kill", name])
+    except Exception as e:
+        print(e)
+    try:
+        subprocess.run(["docker", "container", "rm", name])
+    except Exception as e:
+        print(e)
+
+
+##############################################################################
+
+# Get available VPNGate providers
 
 items = [read(l) for l in getList().split("\n")[2:] if len(l) > 1000]
 items = sorted([i for i in items if i], key=lambda e: e["score"])
@@ -79,13 +132,16 @@ with tempfile.NamedTemporaryFile(mode="wb+", delete=True) as tempconfig:
     tempconfig.write(picked["ovpn"])
     tempconfig.flush()
 
-    dockerName = "vpngate-%s" % os.urandom(16).hex()
+    dockerName = getName(PROXY)
+
+    killDocker(dockerName) # if any docker exists, kill that first
 
     cmd = buildStartCommand(
         name=dockerName,
         configPath=tempconfig.name,
         daemon=False,
-        socks5Entry=sys.argv[1]
+        socks5Entry=PROXY,
+        inactive=TIMEOUT
     )
 
     print(" ".join(cmd))
@@ -94,9 +150,22 @@ with tempfile.NamedTemporaryFile(mode="wb+", delete=True) as tempconfig:
     pid = p.pid
 
     try:
-        p.wait()
-    except:
-        subprocess.run(["docker", "container", "kill", dockerName])
+        # test connectivity of this VPNGATE
+        count = 0
+        last = time.time()
+        while True:
+            if not testConnectivity(PROXY):
+                print("Warning: seems no internet.")
+                count += time.time() - last
+            else:
+                count = 0
+            last = time.time()
+            if count > TIMEOUT:
+                break
+            time.sleep(INTERVAL)
+
+    finally:
+        killDocker(dockerName)
         p.terminate()
         try:
             os.kill(pid, signal.SIGKILL)
